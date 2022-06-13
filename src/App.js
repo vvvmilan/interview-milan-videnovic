a80613d4f6e2c194accff9e4551927d31aa7b11f
vvvmilan <milan@expostudio.rs>
2022-06-13 17:48:08 +0200

    add POST and DELETE API

--- src/App.js
+++ src/App.js
@@ -9,43 +9,26 @@ import NewTodo from "./www/Components/Wrapper/NewTodo/NewTodo"
 
 function App() {
     const BASE_URL = "https://wiv90yxntf.execute-api.eu-west-1.amazonaws.com/local"
-    // const mockTasks = [
-    //     {
-    //         "id": "aD9STIEBCCeTujiJddMA",
-    //         "created": "2022-06-10T06:34:40.518Z",
-    //         "todo": "edited awesome todo!",
-    //         "done": false
-    //     },
-    //     {
-    //         "id": "UT-TRIEBCCeTujiJudO3",
-    //         "created": "2022-05-05T00:09:17.670Z",
-    //         "todo": "shopping time!",
-    //         "done": true
-    //     },
-    //     {
-    //         "id": "VT-TRIEBCCeTujiJudPU",
-    //         "created": "2022-03-31T22:01:24.988Z",
-    //         "todo": "Et veniam libero odio.",
-    //         "done": false
-    //     },
-    // ]
     const [tasks, setTasks] = useState([]);
-
-    useEffect(() => {
+    const getTasks = () => {
         axios.get(BASE_URL)
             .then(result => {
                 setTasks(result.data.data)
-                console.log(result)
             })
             .catch(error => console.log(error))
-        },
-        [])
+    }
+
+    useEffect(() => {
+        getTasks()
+        }, [])
 
     const handleDelete = (id) => {
-        setTasks(tasks.filter(task => task.id !== id));
+        axios.delete(`${BASE_URL}/${id}`)
+            .then(console.log(`Task ${id} deleted`)
+            )
+            .catch(error => console.log(error))
+        // setTasks(tasks.filter(task => task.id !== id));
     };
-
-
 
   return (
     <div className="App">
@@ -53,6 +36,7 @@ function App() {
         <NewTodo
             tasks={tasks}
             setTasks={setTasks}
+            getTasks={getTasks}
         />
         <TaskList
             tasks={tasks}
--- src/www/Components/Wrapper/NewTodo/NewTodo.js
+++ src/www/Components/Wrapper/NewTodo/NewTodo.js
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 
 import './NewTodo.css';
+import axios from "axios";
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -11,13 +12,18 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'left',
 }));
 
-function NewTodo({ tasks, setTasks }) {{
+function NewTodo({ tasks, setTasks, getTasks }) {{
     const handleSubmit = (e) => {
         e.preventDefault();
-        setTasks([...tasks, {
-            todo: e.target[0].value,
-            done: false,
-        }])
+        e.target[0].value.length
+            ? axios.post("https://wiv90yxntf.execute-api.eu-west-1.amazonaws.com/local", {
+                "todo": e.target[0].value,
+                "done": false
+            })
+                .then(getTasks()
+                )
+                .catch(error => console.log(error))
+            : alert(`Please name your task!`)
         e.target[0].value = "";
     }
 
@@ -32,12 +38,17 @@ function NewTodo({ tasks, setTasks }) {{
                 variant="outlined"
                 className='inputTask'
             />
-            <Button
-                variant="text"
-                className="btnAdd"
-            >
-                Add
-            </Button>
+            <input type="submit"
+                   value="ADD"
+                   className="btnAdd"
+            />
+            {/*<Button*/}
+            {/*    variant="text"*/}
+            {/*    className="btnAdd"*/}
+            {/*    onClick={handleSubmit}*/}
+            {/*>*/}
+            {/*    Add*/}
+            {/*</Button>*/}
         </form>
     );
 }
