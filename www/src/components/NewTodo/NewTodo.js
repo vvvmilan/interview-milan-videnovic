4eb435e32ece9c96e4ab99d07d96bb8b55f3bcf2
Milan EXPO <milanexpo@Milans-MacBook-Pro.local>
2022-06-16 22:46:02 +0200

    fix folder structure

---
+++ www/src/components/NewTodo/NewTodo.js
@@ -0,0 +1,97 @@
+import { useContext } from "react";
+
+import { styled } from '@mui/material/styles';
+import axios from "axios";
+import Button from "@mui/material/Button";
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { AppContext } from "../../providers/AppProvider";
+
+import './NewTodo.css';
+import {BASE_URL} from "../../config";
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+}));
+
+function NewTodo() {{
+    const { tasks, setTasks, setIsLoading } = useContext(AppContext)
+
+    const handleSubmit = (e) => {
+        setIsLoading(true);
+        e.preventDefault();
+        const newTask = {
+            "todo": e.target[0].value,
+            "done": false
+        }
+        e.target[0].value.length
+            ? axios.post(BASE_URL, newTask)
+                .then((response) => {
+                    console.log(response)
+                    setTasks([
+                        {
+                            ...newTask,
+                            id: response.data.id
+                        },
+                        ...tasks,
+                        ])
+                    setIsLoading(false);
+                    }
+                )
+                .catch(error => console.log(error))
+            : alert(`Please name your task!`)
+        e.target[0].value = "";
+    }
+/*    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTasks([...tasks, {
+            todo: e.target[0].value,
+            done: false,
+        }])
+        e.target[0].value = "";
+    }*/
+
+    return (
+        <form
+            className='newTodo'
+            onSubmit={handleSubmit}
+        >
+            <TextField
+                id="outlined-basic"
+                label="Add new task"
+                variant="outlined"
+                className='inputTask'
+            />
+            <input type="submit"
+                   value="ADD"
+                   className="btnAdd"
+            />
+        </form>
+    );
+}
+
+
+
+/*    return (
+        <form
+            className='newTodo'
+            onSubmit={handleSubmit}
+        >
+            <TextField
+                id="outlined-basic"
+                label="Add new task"
+                variant="outlined"
+                className='inputTask'
+            />
+            <Button
+                variant="text"
+                className="btnAdd"
+            >
+                Add
+            </Button>
+        </form>*/
+}
+
+export default NewTodo;
